4efacd6cc96bc9503cfe07798ad4dbf5928c738a
ChristinaGaitan <christina.gaitan@gmail.com>
2021-02-03 09:16:11 -0600

    Fix typos in different files

--- basic_app/urls.py
+++ basic_app/urls.py
@@ -6,5 +6,5 @@ from basic_app import views
 app_name = 'basic_app'
 
 urlpatterns = [
-    path('register/', views.register, name='register')
+    path('register/', views.registration, name='register')
 ]
